ee582e461224af58194c7561d2fc59113c2d7826
MarcusKoseckFaculty <mkoseck@mchenry.edu>
2024-08-17 17:34:12 -0500

    Add files via upload

---
+++ print_statement.py
@@ -0,0 +1,12 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")  
+print("Who's there?")  
+print("Olive.")  
+print("Olive \nwho?")  
+print("Olive \'your\' jokes are bad!")  
+
+print()
+# As one line of code using escape characters
+print("Knock, knock.\nWho's there?\nOlive.\nOlive who?\nOlive your jokes are bad!")
+
+print("Python","is","Cool",sep="~~~~~")
